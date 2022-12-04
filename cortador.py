83e15e7ca847911d131013a9a84fc31086e69356
GaVaACJA <gvalverde99@gmail.com>
2022-12-04 17:46:25 -0300

    Agregue content managers

--- cortador.py
+++ cortador.py
@@ -9,13 +9,14 @@ RUTA_DESTINO: str = "Clips/"
 PREFIJO: str = "prueba"
 EXTENSION: str = ".mp4"
 
-
-VIDEO_ORIGINAL=VideoFileClip(VIDEO)
-class response():
-    def __init__(self, exito: bool, clip: VideoFileClip, error: str = "") -> None:
-        self.exito = exito
-        self.clip = clip
-        self.error = error
+class subclip():
+    def __init__(self, nombre:str, inicio:int, final:int) -> None:
+        self.nombre = nombre
+        self.inicio = inicio
+        self.final = final
+        self.clip: VideoFileClip
+    def aplicar_corte(self, original: VideoFileClip):
+        self.clip = original.subclip(self.inicio,self.final)
 
 def a_segundos(duracion: str) -> int:
     "Funcion que recibe una duracion en formato MM:SS y devuelve la cantidad de segundos que corresponde a esa duración"
@@ -28,29 +29,29 @@ def nombre_archivo(archivo:dict)->str:
     "Crea el nombre del archivo segun el formato"
     return f"{PREFIJO}_{archivo['No']} {archivo['Nombre']}"
 
-def cortar_video(original: VideoFileClip, segundo_inicio: int, segundo_fin: int) -> response:
-    "Crea un objeto con un fragmento del video original"
-    try:
-        clip=original.subclip(segundo_inicio,segundo_fin)
-    except Exception as e:
-        return response(False,original, str(e))
-    else:
-        return response(True,clip)
+def obtener_parametros(ruta: str):
+    """
+    Dada una ruta a un archivo .csv, crea una lista con objetos subclip sin procesar, pero con la información para hacerlo
 
-with open(TABLA, mode ='r')as FILE:
-    DICT_FILE = csv.DictReader(FILE)
-    resultado: list[bool] =[]
-    for line in DICT_FILE:
-        inicio=a_segundos(line["Inicio"])
-        final=a_segundos(line['Final'])
-        nombre = nombre_archivo(line)
-        corte = cortar_video(VIDEO_ORIGINAL,inicio,final)
-        if corte.exito:
-            corte.clip.write_videofile(RUTA_DESTINO+nombre+EXTENSION)
-            resultado.append(True)
-        else:
-            resultado.append(False)
-            print(f"Error en archivo: {nombre}\nError: {corte.error}")
+    Input
+        ruta (str): La ruta a un archivo .csv
+    Output
+        una list con tantos objetos subclip como filas halla en el csv
+    """
+    output:list[subclip]=[]
+    with open(ruta, mode ='r')as FILE:
+        DICT_FILE = csv.DictReader(FILE)
+        for line in DICT_FILE:
+            inicio=a_segundos(line["Inicio"])
+            final=a_segundos(line['Final'])
+            nombre = nombre_archivo(line)
+            output.append(subclip(nombre,inicio,final))
+    return output
 
-if all(resultado):
-    print("Cortes realizados con exito")
+if __name__=="__main__":
+    clips = obtener_parametros(TABLA)
+    with VideoFileClip(VIDEO) as ORIGINAL:
+        for corte in clips:
+            corte.aplicar_corte(ORIGINAL)
+            corte.clip.write_videofile(RUTA_DESTINO+corte.nombre+EXTENSION)
+            corte.clip.close()
