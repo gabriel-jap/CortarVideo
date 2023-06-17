f218a932761d42d1792d6340ce98966bc94d4700
GaVaACJA <gvalverde99@gmail.com>
2023-06-16 22:24:29 -0300

    Correcion de nombre de archivo

--- constants.py
+++ constants.py
@@ -4,5 +4,5 @@ nombre_del_video = "w_LSA_202304_02_r720P.mp4"
 
 
 RUTA_DEL_VIDEO_ENTERO = os.path.join("Clips",nombre_del_video)
-RUTA_DESTINO_DE_LISTA = os.path.join("Clips","Tiempos - 1_0.csv")
+RUTA_DESTINO_DE_LISTA = os.path.join("Clips","Tiempos.csv")
 RUTA_DESTINO= os.path.join("Clips","")
